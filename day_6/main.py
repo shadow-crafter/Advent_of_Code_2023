dec181360b8c0eec50272f9a5407c09ca088a6e0
Shadowcrafter2017 <sharkpuncher08@gmail.com>
2023-12-06 15:46:17 -0500

    day 6 finished

--- day_6/main.py
+++ day_6/main.py
@@ -1,32 +1,36 @@
 import math
 
-times = []
-distances = []
+races = [[], []]
+real_race = [[], []]
 
 def process_input(line):
     split1 = line.strip().split(' ')
     converted = list(map(lambda x: int(x) if x.isnumeric() else None, split1))
-    filtered = [x for x in converted if x != None]
+    filtered_race = [x for x in converted if x != None]
 
-    return filtered
+    split2 = line.strip().split(':')[1].replace(' ', '')
+    combined = [int(''.join(split2))]
 
-def get_wins():
+    return filtered_race, combined
+
+def get_wins(race):
     win_methods = []
-    for i, time in enumerate(times):
+    for i, time in enumerate(race[0]):
         wins_possible = 0
         for ms_held in range(time):
             distance_traveled = ms_held * (time - ms_held)
-            if distance_traveled > distances[i]:
+            if distance_traveled > race[1][i]:
                 wins_possible += 1
         win_methods.append(wins_possible)
 
     return win_methods
 
 with open("input.txt") as f:
-    times = process_input(f.readline())
-    distances = process_input(f.readline())
+    races[0], real_race[0] = process_input(f.readline())
+    races[1], real_race[1] = process_input(f.readline())
 
-wins_possible = get_wins()
+wins_possible = math.prod(get_wins(races))
+real_wins_possible = math.prod(get_wins(real_race))
 
-print(f"Times: {times} \nDistances: {distances} \nWin List: {wins_possible}")
-print(f"\nAnswer: {math.prod(wins_possible)}")
+print(f"Times: {races[0]} \nDistances: {races[1]} \nWin List: {wins_possible}")
+print(f"\nAnswer: {wins_possible}, Real: {real_wins_possible}")
